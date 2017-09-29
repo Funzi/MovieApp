148bb326953a9b7cbe6946297853605a61f51390
David Pribula <davidpribula@gmail.com>
2017-09-29 10:04:51 +0200

    Creating project

---
+++ Movio/app/src/main/java/uco_396575/movio2/pv256/fi/muni/cz/movio/MainActivity.java
@@ -0,0 +1,13 @@
+package uco_396575.movio2.pv256.fi.muni.cz.movio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
